fe72fd421baf7f9d91a64b3665595c95989da7f1
Mattias Persson <mattias@mevolution.se>
2013-06-30 23:44:32 +0200

    Skit skumt!!!!

--- README.md
+++
@@ -1,4 +0,0 @@
-filemanager
-===========
-
-A restful filemanager in php using AngularJs as frontend. Please forgive the terrible mix of Swedish and English in code and comments :-)
--- phpinfo.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by JetBrains PhpStorm.
- * User: mattias
- * Date: 2013-01-06
- * Time: 23:11
- * To change this template use File | Settings | File Templates.
- */
-phpinfo();
